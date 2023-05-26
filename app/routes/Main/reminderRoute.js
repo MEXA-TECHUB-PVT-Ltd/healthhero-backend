8b7bcb07ba4ff0a1ea06610d43ae0793008a9755
Usama <uk5458622@gmail.com>
2023-05-26 12:17:51 +0500

    reminder apis updated , active-status apis

--- app/routes/Main/reminderRoute.js
+++ app/routes/Main/reminderRoute.js
@@ -8,6 +8,10 @@ router.put("/update_reminder" , controller.update_reminder);
 router.delete("/deleteReminder" , controller.deleteReminder);
 router.get("/get_reminder" , controller.get_reminder);
 router.get("/getAllUserReminders" , controller.getAllUserReminders);
+router.put("/active_reminder" , controller.active_reminder);
+router.put("/in_active_reminder" , controller.in_active_reminder);
+
+
 
 
 
