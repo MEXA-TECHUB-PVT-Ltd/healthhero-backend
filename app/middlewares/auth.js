11cbeace649cdc91999d7e360cfb36d52e7ad654
= <=>
2023-09-15 11:21:33 +0500

    image upload issue resolved

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 module.exports = (req, res, next) => {
+  console.log(req.headers.authorization)
   try {
     if(!req.headers.authorization){
       return(
